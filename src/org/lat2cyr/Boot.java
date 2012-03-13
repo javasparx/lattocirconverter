7112cecac7ee9beb9941988009ce2c99b9e48df4
zeljic <zeljic@5147aa17-d3d4-26c6-a0c6-5d2bfae62657>
2012-03-13 19:42:43 +0000

    New icon set. New package structure. Changed panel structure on mainPane

---
+++ src/org/lat2cyr/Boot.java
@@ -0,0 +1,34 @@
+package org.lat2cyr;
+
+import org.lat2cyr.tpl.MainPane;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Djordje Zeljic
+ */
+public class Boot extends Application {
+
+    private Stage primaryStage = null;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // init primaryScene
+        this.primaryStage = primaryStage;
+        initPrimaryScene();
+
+        primaryStage.setScene(new Scene(new MainPane(), 600, 400));
+        primaryStage.show();
+    }
+
+    private void initPrimaryScene() {
+        this.primaryStage.setTitle("Lat2Cir Converter");
+        this.primaryStage.setResizable(true);
+    }
+
+}
