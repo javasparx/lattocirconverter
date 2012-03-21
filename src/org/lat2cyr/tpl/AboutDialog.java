b278b9dd595aee62b6e53c8ad7e3f960d8957bd1
zoranp90 <zoranp90@gmail.com>
2012-03-21 18:53:51 +0000

    Fix logo image position on AboutDialog.

--- src/org/lat2cyr/tpl/AboutDialog.java
+++ src/org/lat2cyr/tpl/AboutDialog.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,8 +15,11 @@ import javafx.scene.effect.Lighting;
 import javafx.scene.effect.Reflection;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -38,32 +42,41 @@ public class AboutDialog extends Stage {
 	public void open(){
 		
 		Group root = new Group();
+		
+		//Adding BorderPane
+		BorderPane border = new BorderPane();
+		
+		//Adding GridPane
 		GridPane gridPane = new GridPane();
 		gridPane.setPadding(new Insets(7));
-		gridPane.setHgap(5);
-		gridPane.setVgap(5);
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
 		
-		Image image = new Image("src/org/lat2cyr/resources/icons/logo.png");
-		ImageView iv1 = new ImageView();
-        iv1.setImage(image);
-       
-        
-        //Adding shadow to object
+		//Addding VBOX
+		VBox vbox = new VBox();
+		vbox.setPadding(new Insets(10, 10, 10, 10));
+		vbox.setSpacing(10);
+		
+		//Adding shadow to object
         DropShadow ds = new DropShadow();
         ds.setOffsetY(3.0);
         ds.setOffsetX(3.0);
-        ds.setColor(Color.GRAY);
+        ds.setColor(Color.GRAY);		
+		
+		
+		//Logo image
+		Image image = new Image("src/org/lat2cyr/resources/icons/logo.png");
+		ImageView iv1 = new ImageView();
+        iv1.setImage(image);
         
+        //Adding  ImageView to VBOX           
+        vbox.getChildren().add(iv1);
         
-     
- 
-      
-		//GridPane.setConstraints(iv1, 0, 0);
-		gridPane.add(iv1, 0, 0);
+           
+		//GridPane components
 		GridPane.setMargin(lblTitle, new Insets(0, 50, 0, 50));
 		lblTitle.setFont(new Font("Arial", 20.0));
 		lblTitle.setEffect(ds);
-		
 		gridPane.add(lblTitle, 1, 0);
 		lblVersion.setEffect(ds);
 		gridPane.add(lblVersion, 1, 1);
@@ -75,26 +88,35 @@ public class AboutDialog extends Stage {
 		btnClose.setPrefSize(80, 25);
 		gridPane.add(btnClose, 1, 4);
 		GridPane.setHalignment(btnClose, HPos.RIGHT);
-		root.getChildren().add(gridPane);
+		
+		//Reflection and adding reflection to ROOT
 		Reflection r = new Reflection();
-	      r.setFraction(0.1);
-	      root.setEffect(r);
+	    r.setFraction(0.1);
+	    root.setEffect(r);
 		
 		btnClose.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 			close();
+			System.out.println("What?");
 			}
 		});
 		
+		//Add VBOX on left side of BorderPane
+		border.setLeft(vbox); 
 		
-	
+		//Add GridPane to center of BorderPane
+		border.setCenter(gridPane);
 		
+		//Add VBOX, BorderPane and GridPane to ROOT 
+		root.getChildren().add(gridPane);
+        root.getChildren().add(vbox);
+        root.getChildren().add(border);
+      	
+      	
 		Scene scene = new Scene(root);
-		//Scene sc = new Scene(gridPane);
-		
 		this.setTitle("About");
 		this.setScene(scene);
-		this.setResizable(false);
+		//this.setResizable(false);
 		this.show();
 		
 		
