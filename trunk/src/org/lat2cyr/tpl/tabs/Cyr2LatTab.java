986bc4713b09962dd8237ec420b9456834194f10
zeljic <zeljic@5147aa17-d3d4-26c6-a0c6-5d2bfae62657>
2012-03-27 22:21:38 +0000

    Added Clear All button. Changed clear icon

--- trunk/src/org/lat2cyr/tpl/tabs/Cyr2LatTab.java
+++ trunk/src/org/lat2cyr/tpl/tabs/Cyr2LatTab.java
@@ -2,8 +2,9 @@ package org.lat2cyr.tpl.tabs;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 import org.lat2cyr.Boot;
 import org.lat2cyr.tpl.MessageBox;
 import org.lat2cyr.tpl.MessageBox.MessageBoxType;
@@ -11,7 +12,6 @@ import org.lat2cyr.tpl.toolbars.TabToolbar;
 import org.lat2cyr.utils.Converter;
 import org.lat2cyr.utils.Converter.ConvertType;
 import org.lat2cyr.utils.I18n;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -135,14 +135,17 @@ public class Cyr2LatTab extends Tab {
 					BufferedReader br = null;
 
 					try {
-						br = new BufferedReader(new FileReader(importFile));
+						br = new BufferedReader(new InputStreamReader(new FileInputStream(importFile)));
+
 						String line = null;
 						String text = "";
 
-						while((line = br.readLine()) != null)
-							text = text.concat(line).concat(System.getProperty("line.separator"));
+						String nl = System.getProperty("line.separator", "\n");
 
-						sourceTx.setText(text.trim());
+						while((line = br.readLine()) != null)
+							text += line + nl;
+
+						sourceTx.setText( text.trim() );
 
 					} catch (Exception e) {
 						MessageBox.show(MessageBoxType.ERROR, I18n.localize("File Error"), I18n.localize("Error while reading content from selected file"));
@@ -201,6 +204,14 @@ public class Cyr2LatTab extends Tab {
 
 				}else
 					MessageBox.show(MessageBoxType.ERROR, I18n.localize("File Error"), I18n.localize("Can't write to selected file"));
+			}
+		});
+
+		toolbar.clearBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				sourceTx.clear();
+				convertTx.clear();
 			}
 		});
 
