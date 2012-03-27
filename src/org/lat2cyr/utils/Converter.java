bf81da5329b3926bf5770aab0af52c0b1b406906
zeljic <zeljic@5147aa17-d3d4-26c6-a0c6-5d2bfae62657>
2012-03-27 18:15:50 +0000

    Better convert method

--- src/org/lat2cyr/utils/Converter.java
+++ src/org/lat2cyr/utils/Converter.java
@@ -14,14 +14,11 @@ public class Converter {
 	}
 
 	private void initList() {
-		String lat = "DŽ LJ NJ A B C Č Ć D Đ E F G H I J K L M N O P R S Š T U V Z Ž";
-		lat += " " + lat.toLowerCase();
-		lat += " Dž Lj Nj";
+		String _lat = "A B C Č Ć D Đ E F G H I J K L M N O P R S Š T U V Z Ž";
+		String lat = "Dž DŽ dž Lj LJ lj Nj NJ nj " + _lat + " " + _lat.toLowerCase();
 
-
-		String cyr = "Џ Љ Њ А Б Ц Ч Ћ Д Ђ Е Ф Г Х И Ј К Л М Н О П Р С Ш Т У В З Ж";
-		cyr += " " + cyr.toLowerCase();
-		cyr += " Џ Љ Њ";
+		String _cyr = "А Б Ц Ч Ћ Д Ђ Е Ф Г Х И Ј К Л М Н О П Р С Ш Т У В З Ж";
+		String cyr = "Џ Џ џ Љ Љ љ Њ Њ њ " + _cyr + " " + _cyr.toLowerCase();
 
 		listLat = lat.split(" ");
 		listCyr = cyr.split(" ");
