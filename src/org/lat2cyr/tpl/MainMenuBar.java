57900e03bb5e27d040ae08e7ad3d27e980e541ba
zoranp90 <zoranp90@gmail.com>
2012-03-28 23:01:42 +0000

    Added actions to all menu items.

--- src/org/lat2cyr/tpl/MainMenuBar.java
+++ src/org/lat2cyr/tpl/MainMenuBar.java
@@ -1,66 +1,148 @@
 package org.lat2cyr.tpl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import org.lat2cyr.Boot;
+import org.lat2cyr.tpl.MessageBox.MessageBoxType;
+import org.lat2cyr.tpl.tabs.Lat2CyrTab;
+import org.lat2cyr.tpl.toolbars.TabToolbar;
 import org.lat2cyr.utils.I18n;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooserBuilder;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
 
 public class MainMenuBar extends MenuBar {
 
-	//File menu
+	private Tab lat2CyrTab = new Lat2CyrTab();
+	// File menu
 	public Menu fileMn = new Menu(I18n.localize("File"));
-	public Menu openMn = new Menu(I18n.localize("Open"));
-	public MenuItem fromLatin = new MenuItem(I18n.localize("From Latin Source"));
-	public MenuItem fromCyrilic = new MenuItem(I18n.localize("From Cyrillic Source"));
-	public Menu saveMn = new Menu(I18n.localize("Save"));
-	public MenuItem latinSave = new MenuItem(I18n.localize("as Latin"));
-	public MenuItem cyrilicSave = new MenuItem(I18n.localize("as Cyrillic"));
+	public MenuItem importMn = new MenuItem(I18n.localize("Import"));
+	public MenuItem exportMn = new MenuItem(I18n.localize("Export"));
+	public MenuItem convertMn = new MenuItem(I18n.localize("Convert"));
 	public MenuItem exitMn = new MenuItem(I18n.localize("Exit"));
 
-	//Edit menu
+	// Edit menu
 	public Menu editMn = new Menu(I18n.localize("Edit"));
 	public Menu copyMn = new Menu(I18n.localize("Copy"));
 	public MenuItem copyLatin = new MenuItem(I18n.localize("Latin text"));
 	public MenuItem copyCyrilic = new MenuItem(I18n.localize("Cyrillic text"));
-	public Menu pasteMn = new Menu(I18n.localize("Paste"));
-	public MenuItem pasteLatin = new MenuItem(I18n.localize("Latin text"));
-	public MenuItem pasteCyrilic = new MenuItem(I18n.localize("Cyrillic text"));
-	public Menu clearMn = new Menu(I18n.localize("Clear"));
-	public MenuItem clearLatin = new MenuItem(I18n.localize("Latin text"));
-	public MenuItem clearCyrilic = new MenuItem(I18n.localize("Cyrillic text"));
+	public MenuItem pasteMn = new MenuItem(I18n.localize("Paste"));
+	public MenuItem clearMn = new MenuItem(I18n.localize("Clear all"));
 
-	//Help menu
+	// Help menu
 	public Menu helpMn = new Menu(I18n.localize("Help"));
 	public MenuItem optionsMn = new MenuItem(I18n.localize("Options"));
 	public MenuItem aboutMn = new MenuItem(I18n.localize("About"));
 
-
-
 	public MainMenuBar() {
 
-		//Adding components to File menu
-		openMn.getItems().addAll(fromLatin, fromCyrilic);
-		saveMn.getItems().addAll(latinSave, cyrilicSave);
-		fileMn.getItems().addAll(openMn, saveMn, new SeparatorMenuItem(), exitMn);
+		// Adding components to File menu
 
-		//Adding components to Edit menu
+		fileMn.getItems().addAll(importMn, exportMn, new SeparatorMenuItem(),
+				convertMn, new SeparatorMenuItem(), exitMn);
+
+		// Adding components to Edit menu
 		copyMn.getItems().addAll(copyLatin, copyCyrilic);
-		pasteMn.getItems().addAll(pasteLatin, pasteCyrilic);
-		clearMn.getItems().addAll(clearLatin, clearCyrilic);
-		editMn.getItems().addAll(copyMn, pasteMn,new SeparatorMenuItem(), clearMn);
+		editMn.getItems().addAll(copyMn, pasteMn, clearMn);
 
-		//Adding components to Help menu
+		// Adding components to Help menu
 		helpMn.getItems().addAll(optionsMn, new SeparatorMenuItem(), aboutMn);
 
-
-		//Adding File, Edit, Help menu to MenuBar
+		// Adding File, Edit, Help menu to MenuBar
 		this.getMenus().add(fileMn);
 		this.getMenus().add(editMn);
 		this.getMenus().add(helpMn);
 
+		menuActions();
+
 	}
 
+	public void menuActions() {
 
+		// File menu actions
+
+		importMn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				Lat2CyrTab.getToolbar().importBtn.fireEvent(new ActionEvent());
+			}
+		});
+
+		exportMn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				Lat2CyrTab.getToolbar().exportBtn.fireEvent(new ActionEvent());
+			}
+		});
+
+		convertMn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				Lat2CyrTab.getToolbar().convertBtn.fireEvent(new ActionEvent());
+			}
+		});
+
+		exitMn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				System.exit(0);
+			}
+		});
+
+		// Edit menu actions
+		copyLatin.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				Lat2CyrTab.getToolbar().copySourceBtn.fireEvent(new ActionEvent());
+			}
+		});
+		
+		copyCyrilic.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				Lat2CyrTab.getToolbar().copyConvertBtn.fireEvent(new ActionEvent());
+			}
+		});
+		
+		pasteMn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				Lat2CyrTab.getToolbar().pasteBtn.fireEvent(new ActionEvent());
+			}
+		});
+		
+
+		clearMn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent t) {
+				Lat2CyrTab.getToolbar().clearBtn.fireEvent(new ActionEvent());
+			}
+		});
+
+		// Help menu actions
+		optionsMn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				new Options().open();
+			}
+		});
+
+		aboutMn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				AboutDialog ad = new AboutDialog();
+				ad.open();
+			}
+		});
+
+	}
 
 }
